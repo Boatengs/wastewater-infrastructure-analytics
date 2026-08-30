e0145a3d4fe41dea0603418a94a68a3716f8183e
Sam Boateng <32736261+Boatengs@users.noreply.github.com>
2026-08-30 16:31:56 -0400

    Add package initializer

---
+++ src/wastewater_infrastructure_analytics/__init__.py
@@ -0,0 +1,6 @@
+"""Wastewater infrastructure analytics package."""
+
+from .risk import classify_risk, risk_priority_number
+
+__all__ = ["classify_risk", "risk_priority_number"]
+__version__ = "0.1.0"
